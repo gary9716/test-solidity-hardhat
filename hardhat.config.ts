6779d21faa05a7a9e66f496b31112518d95066c0
Gary <gary@aifian.com>
2025-01-26 11:46:32 +0800

    init commit

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@typechain/hardhat';
+import '@nomicfoundation/hardhat-ethers';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.28',
+  typechain: {
+    outDir: 'typechain-types',
+    target: 'ethers-v6',
+  },
+};
+
+export default config;
